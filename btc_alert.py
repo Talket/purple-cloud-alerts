3269a152bbbcc30c9768d63e65f8546962946a6a
Talket <talke.dobariya2024@lpu.in>
2026-05-31 12:29:50 +0530

    Update btc_alert.py

--- btc_alert.py
+++ btc_alert.py
@@ -1,10 +1,11 @@
 import requests
+import pandas as pd
 
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
 
 params = {
     "vs_currency": "usd",
-    "days": "7",
+    "days": "30",
     "interval": "hourly"
 }
 
@@ -12,5 +13,9 @@ data = requests.get(url, params=params).json()
 
 prices = data["prices"]
 
-print(f"Downloaded {len(prices)} candles")
-print(prices[-1])
+df = pd.DataFrame(prices, columns=["timestamp", "close"])
+
+df["EMA200"] = df["close"].ewm(span=200).mean()
+
+print("Current Price:", round(df["close"].iloc[-1], 2))
+print("EMA200:", round(df["EMA200"].iloc[-1], 2))
